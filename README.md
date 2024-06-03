faffb9edf2bccc88fc22fb04db551b8b4f0a45d5
QuiMir <2546670299@qq,com>
2024-06-03 17:41:01 +0800

    Build OpenGL/

---
+++ README.md
@@ -0,0 +1,12 @@
+# Build OpenGL using CMake
+
+## Project introduction
+This project is [LearnOpenGL](https://learnopengl.com/) to learn the generated code and add some of your own insights. 
+The project follows the project[Apache v2.0](https://www.apache.org/licenses/LICENSE-2.0) agreement. Code style 
+compliance[Google's open source style guide](https://zh-google-styleguide.readthedocs.io/en/latest/google-cpp-styleguide/).
+
+## Project compilation environment
+ - Window
+ - MSVC 17.0
+ - CMake 3.24.2
+ - CXX 17
---
+++ src/started_opengl/main.cc
@@ -0,0 +1,75 @@
+﻿/*******************************************************************************
+ * Copyright 2024 QuiMir
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+void FrameBufferSizeCallback(GLFWwindow* window,int width,int height)
+{
+  glViewport(0,0,width,height);
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+  if (glfwGetKey(window,GLFW_KEY_ESCAPE)==GLFW_PRESS)
+  {
+	glfwSetWindowShouldClose(window,true);
+  }
+}
+
+int main()
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+  
+  GLFWwindow* window= glfwCreateWindow(800,600,"LearnOpenGL", nullptr, nullptr);
+
+  if (window== nullptr)
+  {
+	cout<<"Failed to create GLFW window"<<endl;
+	glfwTerminate();
+	return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+  {
+	cout<<"Failed to initialize GLAD"<<endl;
+	return -1;
+  }
+
+  glfwSetFramebufferSizeCallback(window,FrameBufferSizeCallback);
+  
+  while(!glfwWindowShouldClose(window))
+  {
+
+	ProcessInput(window);
+
+	glClearColor(0.2f,0.3f,0.3f,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwPollEvents();
+	glfwSwapBuffers(window);
+  }
+  
+  glfwTerminate();
+  return 0;
+}
